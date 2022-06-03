621808c8b22a3c2710947379d3024496aea4740b
Rodrigo Érico <rodrigoerico25@gmail.com>
2022-06-03 10:49:43 -0300

    Criado Product no banco de dados.

--- django-sqlite3/core/models.py
+++ django-sqlite3/core/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField('Nome', max_length=100)
+    price = models.DecimalField('Preço', decimal_places=2, max_digits=8)
+    stock = models.IntegerField('Quantidade em Estoque')
