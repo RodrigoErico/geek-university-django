2c32a17fb04bdff1721d3fd75a130479279d2bff
Rodrigo Érico <rodrigoerico25@gmail.com>
2022-06-03 15:16:36 -0300

    Criado e adicionado tabela Client ao banco de dados.

--- django-sqlite3/core/models.py
+++ django-sqlite3/core/models.py
@@ -4,3 +4,9 @@ class Product(models.Model):
     name = models.CharField('Nome', max_length=100)
     price = models.DecimalField('Preço', decimal_places=2, max_digits=8)
     stock = models.IntegerField('Quantidade em Estoque')
+
+
+class Client(models.Model):
+    name = models.CharField('Nome', max_length=100)
+    last_name = models.CharField('Sobrenome', max_length=100)
+    email = models.EmailField('E-mail', max_length=100)
