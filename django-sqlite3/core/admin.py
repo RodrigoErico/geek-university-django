fee71ec118fd3448cd26f12798f74f96f5d4dc70
Rodrigo Érico <rodrigoerico25@gmail.com>
2022-06-03 15:47:54 -0300

    Criado funções para mostrar os dados na pagina django.

--- django-sqlite3/core/admin.py
+++ django-sqlite3/core/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Product, Client
 
-admin.site.register(Product)
-admin.site.register(Client)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+    
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+    
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Client, ClientAdmin)
