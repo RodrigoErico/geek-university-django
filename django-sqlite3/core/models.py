6da0937fd0f4dd533e7994cfb796f86fca46c028
Rodrigo Érico <rodrigoerico25@gmail.com>
2022-06-03 15:40:09 -0300

    Criado clientes e produtos no banco de dados.

--- django-sqlite3/core/models.py
+++ django-sqlite3/core/models.py
@@ -5,8 +5,15 @@ class Product(models.Model):
     price = models.DecimalField('Preço', decimal_places=2, max_digits=8)
     stock = models.IntegerField('Quantidade em Estoque')
 
-
+    
+    def __str__(self):
+        return f'{self.name}'
+    
 class Client(models.Model):
     name = models.CharField('Nome', max_length=100)
     last_name = models.CharField('Sobrenome', max_length=100)
     email = models.EmailField('E-mail', max_length=100)
+    
+    
+    def __str__(self):
+        return f'{self.name} {self.last_name}'
