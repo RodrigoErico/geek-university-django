7e258d41de49387f18244a83559123d3c22cd1ab
Rodrigo Érico <rodrigoerico25@gmail.com>
2022-06-07 10:36:47 -0300

    Criado a views da aplicação.

--- django-mysql/core/views.py
+++ django-mysql/core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'contato.html')
+
+def product(request):
+    return render(request, 'produto.html')
