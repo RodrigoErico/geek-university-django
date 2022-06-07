768d6b432b197718335fd6446b226ae2596a0b3a
Rodrigo Érico <rodrigoerico25@gmail.com>
2022-06-07 11:01:57 -0300

    Criado a rotas.

---
+++ django-mysql/core/urls.py
@@ -0,0 +1,9 @@
+from unicodedata import name
+from django.urls import path 
+from .views import index, contact, product
+
+urlpatterns = [
+    path('', index, name='index')
+    path('contato/', contact, name='contact')
+    path('produto/', product, name='product')
+]
